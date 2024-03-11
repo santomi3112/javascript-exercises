cf1511d863e5400e4cde522a549313d0308b4ca1
Tomi <amandipthandi@gmail.com>
2024-03-11 20:57:03 +0700

    Add reverseString implementation and update test cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  const charArray = str.split('');
 
+  // Reverse the array manually
+  const reversedCharArray = [];
+  for (let i = charArray.length - 1; i >= 0; i--) {
+    reversedCharArray.push(charArray[i]);
+  }
+
+  // Join the reversed array into a string
+  const reversedStr = reversedCharArray.join('');
+
+  return reversedStr;
 };
 
 // Do not edit below this line
